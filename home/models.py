d382dfbf77c9a4c17b3bbad2f86c9b814c82eadb
Gabrielaa <gescobar84@misena.edu.co>
2020-10-14 22:52:49 -0500

    actualizacion_BaseDatos_4

--- home/models.py
+++ home/models.py
@@ -1,128 +1,152 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
-class Especialista_profecional(models.Model):
-    nombre = models.CharField(max_length=45)
+''' Tuplas '''
 
-    def __str__(self):
-        return self.nombre
 tipo_identificacion=(
-    ('C','CC'),
-    ('T.I','T.I'),
-    ('Ex','CE'),
+	('C','CC'),
+	('T.I','T.I'),
+	('Ex','CE'),
 )
+
 tipo_doctor= (
-    ('G','GENERAL'),
-    ('E','ESPECIALISTA'),
+	('G','General'),
+	('E','Especialista'),
 )
-class Profesional(models.Model):
-    especialista = models.ForeignKey(Especialista_profecional,on_delete=models.PROTECT)
-    nombre= models.CharField(max_length=45)
-    apellido = models.CharField(max_length=45)
-    tipo_identificacion= models.CharField(choices=tipo_identificacion,max_length=45)
-    identificacion = models.IntegerField()
-    tipo_doctor = models.CharField(choices=tipo_doctor,max_length=45)
-    telefono = models.IntegerField()
-    corro = models.EmailField()
-
-    def __str__(self):
-        return self.nombre
-
 
 estrato = (
-    ('1','Estrato 1'),
-    ('2','Estrato 2'),
-    ('3','Estrato 3'),
-    ('4','Estrato 4'),
-    ('5','Estrato 5'),
-    ('6','Estrato 6'),
+	('1','Estrato 1'),
+	('2','Estrato 2'),
+	('3','Estrato 3'),
+	('4','Estrato 4'),
+	('5','Estrato 5'),
+	('6','Estrato 6'),
 )
+
 nivel_educativo= (
-    ('1','Basica Primaria'),
-    ('2','Basica secundaria'),
-    ('3','Tecnico'),
-    ('4','Tecnologo'),
-    ('5','Universitario'),
-    ('6','Otros'),
+	('1','Basica Primaria'),
+	('2','Basica secundaria'),
+	('3','Tecnico'),
+	('4','Tecnologo'),
+	('5','Universitario'),
+	('6','Otros'),
 )
+
 tipo_paciente= (
-    ('1','Cabeza familia'),
-    ('2','Miembros'),
+	('1','Cabeza familia'),
+	('2','Miembros'),
 )
+
 Tipo_identificacion=(
-    ('C','CC'),
-    ('T.I','T.I'),
-    ('Ex','CE'),
+	('C','CC'),
+	('T.I','T.I'),
+	('Ex','CE'),
 )
-class Paciente(models.Model):
-    estrato= models.CharField(choices=estrato,max_length=45)
-    nivel_educativo=models.CharField(choices=nivel_educativo,max_length=45)
-    correo= models.EmailField()
-    telefono = models.CharField(max_length=45)
-    nombre = models.CharField(max_length=45)
-    apellido = models.CharField(max_length=45)
-    tipo_paciente= models.CharField(choices=tipo_paciente,max_length=50)
-    tipo_identificacion= models.CharField(choices=Tipo_identificacion,max_length=45)
-    identificacion = models.IntegerField()
-    fecha_nacimiento = models.DateField()
-    edad = models.IntegerField()
 
-    def __str__(self):
-        return self.nombre
-
+# estado de la consulta
 estado=(
-    ('Remitida','Remitida'),
-    ('Proceso','Proceso'),
-    ('Rechasada','Rechasada'),
-    ('Terminada','Terminada'),
+	('Remitida','Remitida'),
+	('Proceso','Proceso'),
+	('Rechasada','Rechasada'),
+	('Terminada','Terminada'),
+)
+genero=(
+	('Masculino','Masculino'),
+	('Femenino','Femenino'),
+	('Otro','Otro'),
 )
 
-class Consulta(models.Model):
-    titulo = models.CharField(max_length=45)
-    descripcion = models.TextField(max_length=1000)
-    medico = models.ForeignKey(Profesional,on_delete= models.PROTECT)
-    paciente = models.ForeignKey(Paciente, on_delete= models.PROTECT)
-    estado = models.CharField(choices=estado,max_length=45)
 
-    def __str__(self):
-        return self.titulo
 
-class Grupo_Familia(models.Model):
-    paciente= models.ForeignKey(Paciente, on_delete= models.PROTECT)
-    nombre = models.CharField(max_length=45)
+''' Modelos '''
 
-    def __str__(self):
-        return self.nombre
+class Especialidad (models.Model):
+	nombre = models.CharField(max_length = 45, unique=True)
 
-class Resultado(models.Model):
-    fecha_registro =models.DateField()
-    archivo = models.CharField(max_length=45)
-    consulta= models.ForeignKey(Consulta,on_delete= models.PROTECT)
+	def __str__(self):
+		return self.nombre
 
-    def __str__(self):
-        return self.archivo
+class Profesional (models.Model):
+	nombre				= models.CharField(max_length = 45)
+	apellido			= models.CharField(max_length = 45)
+	tipo_identificacion = models.CharField(choices = tipo_identificacion, max_length = 45)
+	identificacion		= models.CharField(max_length = 45, unique=True)
+	tipo_doctor			= models.CharField(choices = tipo_doctor, max_length = 45)
+	telefono			= models.CharField(max_length = 45)
+	correo				= models.EmailField(unique=True)
+	especialista		= models.ForeignKey(Especialidad, on_delete=models.PROTECT)
+	user 				= models.OneToOneField(User, on_delete = models.PROTECT)
 
-class Toma_Examen(models.Model):
-    consuta= models.ForeignKey(Consulta, on_delete=models.PROTECT)
-    diagnostico = models.TextField(max_length=1000)
-    examen = models.TextField(max_length=1000)
-    procedimiento = models.TextField(max_length=1000)
+	def __str__(self):
+		return self.nombre
 
-    def __str__(self):
-        return self.diagnostico
+class Grupo_Familiar (models.Model):
+	medico_cabecera		= models.CharField(max_length = 45)
 
-class Remision(models.Model):
-    consulta = models.ForeignKey(Consulta, on_delete=models.PROTECT)
-    fecha = models.DateField()
+	def __str__(self):
+		return str(self.id) + ' ' + self.medico_cabecera
 
-    def __str__(self):
-        return self.fecha
+class Paciente (models.Model):
+	nombres				= models.CharField(max_length = 45)
+	apellidos			= models.CharField(max_length = 45)
+	telefono			= models.CharField(max_length = 45, null=True, blank=True)
+	identificacion		= models.CharField(max_length = 45, unique = True)
+	tipo_identificacion = models.CharField(choices = Tipo_identificacion, max_length = 45)
+	correo				= models.EmailField(unique = True)
+	genero				= models.CharField(choices = genero, max_length = 45)
+	
+	estrato				= models.CharField(choices = estrato,max_length = 45)
+	nivel_educativo		= models.CharField(choices = nivel_educativo, max_length = 45)
+	tipo_paciente		= models.CharField(choices = tipo_paciente, max_length = 50)
+	fecha_nacimiento	= models.DateField()
+	edad				= models.IntegerField(null=True, blank=True)
+	grupo_familiar		= models.ForeignKey(Grupo_Familiar, on_delete= models.PROTECT)
+	user 				= models.OneToOneField(User, on_delete = models.PROTECT)
 
-class Incapacidad(models.Model):
-    consulta= models.ForeignKey(Consulta,on_delete=models.PROTECT)
-    descripcion = models.TextField(max_length=1000)
-    fecha_inicio = models.DateField()
-    fecha_fin = models.DateField()
+	def __str__(self):
+		return self.nombre + ' ' + self.identificacion
 
-    def __str__(self):
-        return self.descripcion
+class Consulta (models.Model):
+	fecha_consulta 		= models.DateField(auto_now_add=True)
+	molestia_principal	= models.CharField(max_length = 45)
+	diagnostico 		= models.TextField(max_length = 1000, null = True, blank=True)
+	estado 				= models.CharField(choices = estado, max_length = 45)
+	medico 				= models.ForeignKey(Profesional,on_delete= models.PROTECT)
+	paciente 			= models.ForeignKey(Paciente, on_delete= models.PROTECT)
+
+	def __str__(self):
+		return str(self.fecha_consulta) + ' ' + self.paciente + ' ' + self.estado
+
+# Recomendacion de Examenes a realizar 
+class Toma_Examen (models.Model): 
+	fecha_formulacion	= models.DateField(auto_now_add=True)
+	procedimiento 		= models.TextField(max_length = 1000)
+	consuta 			= models.ForeignKey(Consulta, on_delete=models.PROTECT)
+
+	def __str__(self):
+		return str(self.fecha_formulacion) + ' ' + self.procedimiento 
+
+class Resultado (models.Model):
+	fecha_registro 		= models.DateField()
+	archivo 			= models.FileField(upload_to = 'examenes', null=True, blank=True)
+	consulta 			= models.ForeignKey(Consulta,on_delete= models.PROTECT)
+
+	def __str__(self):
+		return str(self.fecha_formulacion) + ' ' + self.consulta.nombre
+
+
+class Remision (models.Model):
+	consulta 			= models.ForeignKey(Consulta, on_delete=models.PROTECT)
+	fecha 				= models.DateField()
+
+	def __str__(self):
+		return str(self.fecha) + ' ' + self.consulta.nombre
+
+class Incapacidad (models.Model):
+	consulta 			= models.ForeignKey(Consulta,on_delete=models.PROTECT)
+	descripcion 		= models.TextField(max_length = 1000)
+	fecha_inicio 		= models.DateField()
+	fecha_fin 			= models.DateField()
+
+	def __str__(self):
+		return self.descripcion
