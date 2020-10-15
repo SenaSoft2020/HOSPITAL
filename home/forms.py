c53892773a1a207f4f1fd06c7c55899e87cddd8f
Deybison_Perez <dsperez784@misena.edu.co>
2020-10-14 20:01:00 -0500

    actualizacion_#1

--- home/forms.py
+++ home/forms.py
@@ -1,14 +1,15 @@
 from django import forms
-from .models import *
+from django.shortcuts import render
 
 
 class login_form(forms.Form):
-    paciente = forms.CharField(label='Identificacion', widget=forms.TextInput(
+    paciente = forms.CharField(label="Identificacion", widget=forms.TextInput(
         attrs={'class': 'form-control',
                'placeholder': 'Numero de Identificaion'}
     ), min_length=3, max_length=100)
 
-    clave = forms.CharField(label='contraseña', widget=forms.PasswordInput(
-        attrs={'class': 'form-control'}
+    clave = forms.CharField(label="contraseña",widget=forms.PasswordInput(
+        attrs={'class': 'form-control'
+               }
     ))
     # (render_value=False) da error
--- home/views.py
+++ home/views.py
@@ -1,35 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
 from .forms import *
 from .models import *
 from django.contrib.auth import login, logout, authenticate
 # Create your views here.
+
+
 def vista_base(request):
     return render(request, 'base.html')
 
+
 def vista_index(request):
-    
+
     return render(request, 'index.html')
+
 
 def vista_login(request):
     pac = ""
     cla = ""
     if request.method == "POST":
         formulario = login_form(request.POST)
-        pac = formulario.cleaned_data['paciente']
-        cla = formulario.cleaned_data['clave']
-        usuario = autothenticate(username=pas, password=cla)
-        if usuario is not None and usuario.is_activate:
-            login(request,usuario)
-            return redirect('/perfil-u')
-        else:
-            mensaje = "Usuario o clave incorectos"
+        if formulario.is_valid():
+            pac = formulario.cleaned_data['paciente']
+            cla = formulario.cleaned_data['clave']
+            usuario = authenticate(username=pac, password=cla)
+            if usuario is not None and usuario.is_active:
+                login(request, usuario)
+                return redirect('/')
+            else:
+                mensaje = "Usuario o clave incorectos"
     formulario = login_form()
-    return render(request, 'login.html',locals())
+    return render(request, 'login.html', locals())
+
 
 def vista_logout(request):
     logout(request)
     return redirect('/login/')
+
 
 def vista_perfil(request):
     return render(request, 'perfil-u.html')
@@ -42,11 +49,14 @@ def vista_cita(request):
 def vista_examen(request):
     return render(request, 'examen.html')
 
+
 def vista_medico(request):
     return render(request, 'perfil-a.html')
 
+
 def vista_pacientes(request):
     return render(request, 'pacientes.html')
+
 
 def vista_horario(request):
     return render(request, 'horario.html')
